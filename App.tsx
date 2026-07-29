21dc922b48fbd103292dadc4a71657d935db0af0
HeofonGlobalSearch <heofonsearch.help@outlook.com>
2026-07-29 17:51:13 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,43 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SpaceBackground from "./components/SpaceBackground";
-import NavBar from "./components/NavBar";
+import SpaceBackground from "./SpaceBackground";
+import NavBar from "./NavBar";
 
-import Home from "./pages/Home";
-import SearchResults from "./pages/SearchResults";
-import Heonews from "./pages/Heonews";
-import Heotranslate from "./pages/Heotranslate";
-import Heopad from "./pages/Heopad";
-import Heomaps from "./pages/Heomaps";
-import Heofeedback from "./pages/Heofeedback";
-import Settings from "./pages/Settings";
-import About from "./pages/About";
+import Home from "./Home";
+import SearchResults from "./SearchResults";
+import Heonews from "./Heonews";
+import Heotranslate from "./Heotranslate";
+import Heopad from "./Heopad";
+import Heomaps from "./Heomaps";
+import Heofeedback from "./Heofeedback";
+import Settings from "./Settings";
+import About from "./About";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen relative overflow-x-hidden bg-black text-white">
         <SpaceBackground />
+        <NavBar />
 
-        <header className="relative z-10">
-          <NavBar />
-        </header>
-
-        <main className="relative z-10">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<SearchResults />} />
-
-            <Route path="/heonews" element={<Heonews />} />
-            <Route path="/heotranslate" element={<Heotranslate />} />
-            <Route path="/heopad" element={<Heopad />} />
-            <Route path="/heomaps" element={<Heomaps />} />
-
-            <Route path="/heofeedback" element={<Heofeedback />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/heonews" element={<Heonews />} />
+          <Route path="/heotranslate" element={<Heotranslate />} />
+          <Route path="/heopad" element={<Heopad />} />
+          <Route path="/heomaps" element={<Heomaps />} />
+          <Route path="/heofeedback" element={<Heofeedback />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
