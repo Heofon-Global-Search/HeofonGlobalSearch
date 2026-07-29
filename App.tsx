c6fcca6c65cb71a7ab2df822068b663905790f19
HeofonGlobalSearch <heofonsearch.help@outlook.com>
2026-07-29 17:48:29 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,35 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from '@/components/ui/sonner';
-import SpaceBackground from '@/components/SpaceBackground';
-import NavBar from '@/components/NavBar';
-import Home from '@/pages/Home';
-import SearchResults from '@/pages/SearchResults';
-import Heonews from '@/pages/Heonews';
-import Heotranslate from '@/pages/Heotranslate';
-import Heopad from '@/pages/Heopad';
-import Heomaps from '@/pages/Heomaps';
-import Heofeedback from '@/pages/Heofeedback';
-import Settings from '@/pages/Settings';
-import About from '@/pages/About';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SpaceBackground from "./components/SpaceBackground";
+import NavBar from "./components/NavBar";
+
+import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
+import Heonews from "./pages/Heonews";
+import Heotranslate from "./pages/Heotranslate";
+import Heopad from "./pages/Heopad";
+import Heomaps from "./pages/Heomaps";
+import Heofeedback from "./pages/Heofeedback";
+import Settings from "./pages/Settings";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen relative overflow-x-hidden">
+      <div className="min-h-screen relative overflow-x-hidden bg-black text-white">
         <SpaceBackground />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<SearchResults />} />
-          <Route path="/heonews" element={<Heonews />} />
-          <Route path="/heotranslate" element={<Heotranslate />} />
-          <Route path="/heopad" element={<Heopad />} />
-          <Route path="/heomaps" element={<Heomaps />} />
-          <Route path="/heofeedback" element={<Heofeedback />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Toaster />
+
+        <header className="relative z-10">
+          <NavBar />
+        </header>
+
+        <main className="relative z-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+
+            <Route path="/heonews" element={<Heonews />} />
+            <Route path="/heotranslate" element={<Heotranslate />} />
+            <Route path="/heopad" element={<Heopad />} />
+            <Route path="/heomaps" element={<Heomaps />} />
+
+            <Route path="/heofeedback" element={<Heofeedback />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
       </div>
     </BrowserRouter>
   );
