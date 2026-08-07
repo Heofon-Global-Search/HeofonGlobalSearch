90cc83394ddae116ce45d40fc6c1364f34aa5ae0
HeofonGlobalSearch <heofonsearch.help@outlook.com>
2026-08-07 08:54:43 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "/App.tsx";
-import "../index.css";
-
 ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{
+    color: "white",
+    background: "black",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "50px"
+  }}>
+    Heofon React Works
+  </div>
 );
