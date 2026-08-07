91578ac1d8a9d71877008d3fa41eb6fd73d1f11d
HeofonGlobalSearch <heofonsearch.help@outlook.com>
2026-08-07 08:59:01 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,32 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="*"
-          element={
-            <div style={{
-              background: "black",
-              color: "white",
-              height: "100vh",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column"
-            }}>
-              <h1 style={{fontSize: "70px"}}>
-                Heofon
-              </h1>
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <h1 style={{ fontSize: "70px" }}>
+        Heofon
+      </h1>
 
-              <p>
-                App.tsx is working
-              </p>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+      <p>
+        React is working
+      </p>
+    </div>
   );
 }
