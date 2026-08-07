737aa719101fbb184321dcc6421d8519f76f11f1
HeofonGlobalSearch <heofonsearch.help@outlook.com>
2026-08-07 09:08:03 -0500

    Replace App component with styled header
    Removed App component and added a header with styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
-
 ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 style={{
+    color: "red",
+    fontSize: "60px"
+  }}>
+    TEST FROM MAIN TSX
+  </h1>
 );
